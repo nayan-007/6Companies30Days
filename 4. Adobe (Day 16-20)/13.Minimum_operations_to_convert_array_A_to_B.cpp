53930cd33c51a03883e61196b99e6410c32b0a60
nayan-007 <nayan.kumar.07081999@gmail.com>
2022-01-20 21:10:21 +0530

    added

---
+++ 4. Adobe (Day 16-20)/13.Minimum_operations_to_convert_array_A_to_B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    
+  private:
+  
+  int solve( vector<int> vec){
+      vector<int> res;
+      for(int i=0;i<vec.size();i++){
+          auto it= lower_bound(res.begin(),res.end(),vec[i]);
+          if(it==res.end())
+            res.push_back(vec[i]);
+          else
+            *it=vec[i];
+      }
+      return res.size();
+  }
+  public:
+    int minInsAndDel(int A[], int B[], int N, int M) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<M;i++){
+            mp[B[i]]++;
+        }
+        
+        vector<int>vec;
+        for(int i=0;i<N;i++){
+            if(mp.find(A[i])!=mp.end())
+                vec.push_back(A[i]);
+        }
+        return (N+M) - 2*solve(vec);
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M;
+        cin>>N>>M;
+        
+        int A[N], B[M];
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+        for(int i=0; i<M; i++)
+            cin>>B[i];
+
+        Solution ob;
+        cout << ob.minInsAndDel(A,B,N,M) << endl;
+    }
+    return 0;
+}  
