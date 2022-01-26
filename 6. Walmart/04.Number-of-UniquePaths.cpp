8a731560e88d6376f877e842e42874ff157c330e
nayan-007 <nayan.kumar.07081999@gmail.com>
2022-01-26 20:34:06 +0530

    added

---
+++ 6. Walmart/04.Number-of-UniquePaths.cpp
@@ -0,0 +1,43 @@
+//Initial template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to find total number of unique paths.
+    int NumberOfPath(int a, int b)
+    {
+        int dp[a][b];
+        
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=1;
+                else 
+                    dp[i][j]= dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[a-1][b-1];
+    }
+};
+
+
+
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking dimensions of the matrix
+        int a,b;
+        cin>>a>>b;
+        Solution ob;
+        //calling NumberOfPath() function
+        cout << ob.NumberOfPath(a,b) << endl;
+    }
+}
+
+  // } Driver Code Ends
