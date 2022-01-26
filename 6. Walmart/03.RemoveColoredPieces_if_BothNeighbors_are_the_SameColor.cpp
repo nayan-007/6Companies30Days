31c304eca88f853872cdaeced1e9dca4df38fde0
nayan-007 <nayan.kumar.07081999@gmail.com>
2022-01-26 20:36:01 +0530

    added

---
+++ 6. Walmart/03.RemoveColoredPieces_if_BothNeighbors_are_the_SameColor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int a=0;
+        int b=0;
+        
+        for(int i=1;i<colors.size()-1;i++){
+            if(colors[i]=='A' && colors[i-1]=='A' && colors[i+1]=='A')
+                a++;
+            else if(colors[i]=='B' && colors[i-1]=='B' && colors[i+1]=='B')
+                b++;
+        }
+        return (a>b);
+        
+    }
+};
